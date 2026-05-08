84b2dd390ed8a3db079f5bfefe8675659041cbd9
Gabriel Ojitos Landa <ojitoslanda@gmail.com>
2026-05-08 16:25:55 -0500

    sadasd

---
+++ app/models/Empleados.php
@@ -0,0 +1,23 @@
+<?php 
+//Llamamos a la conexión de la base de datos.
+require_once __DIR__ . '/../core/Database.php';
+//Creamos el modelo o clase llamada Empleado (SINGULAR).
+class Empleado{
+    // La propiedad $db guardará la conexión PDO.
+    // Le decimos que solo puede ser de tipo PDO (tipado estricto).
+    // modificador de acceso("private") significa que solo se puede usar dentro de esta clase.
+    private PDO $db;
+
+    //Al crear el modelo, obtenemos la conexion automaticamente.
+    public function __construct(){
+        // Database::getConnection() nos regresa la conexión PDO que creamos en core/Database.php.
+        // Al guardarla en $this->db, cualquier método de esta clase puede usarla.
+        $this->db = Database::getConnection();
+    }
+    //Creamos el modulo para llamar todo los datos de la tabla EMPLEADOS
+    //public function getAll():array
+    public function obtenerTodo():array{
+        
+    }
+}
+
