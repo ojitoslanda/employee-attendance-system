8a5b55b70702c9ff323366b5a9900266b802b065
Gabriel Ojitos Landa <ojitoslanda@gmail.com>
2026-05-09 14:54:25 -0500

    asdasd

--- app/controllers/EmpleadosController.php
+++ app/controllers/EmpleadosController.php
@@ -5,6 +5,12 @@ require_once __DIR__ . '/../models/Empleado.php';
 class EmpleadosController extends Controller {
     // Método por defecto. 
     public function index(): void {
+        //Mientras que no inicio sesión - que le envie al login
+        if(!isset($_SESSION['usuario'])){
+            header("Location: " . BASE_URL ."/login"); 
+            exit(); //por precausión
+        } 
+
         //Instanciamos el objeto de la clase EMPLEADO;
         $modelo = new Empleado();
         $variable_empleados = $modelo->obtenerEmpleados();
--- app/views/empleados/reportes.php
+++ app/views/empleados/reportes.php
@@ -7,6 +7,8 @@
     <title><?php echo TITLE_BUSINESS; ?> - Empleados</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/css/dashboard.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
@@ -25,10 +27,28 @@
     <div class="main-content">
         <div class="table-responsive">
             <?php print_r($empleados); ?>
+            <table class="table table-bordered text-center align-middle">
+                <thead class="table-dark">
+                    <tr>
+                        <th>Columna 1</th>
+                        <th>Columna 2</th>
+                        <th>Columna 3</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Dato 1</td>
+                        <td>Dato 2</td>
+                        <td>Dato 3</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </main>
 <script src="<?php echo BASE_URL; ?>/public/js/dashboard.js"></script>
+<!-- Bootstrap JS Bundle -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
