15eebbc2bebb04e19afdd9fbfebca3c48d8cee14
Gabriel Ojitos Landa <ojitoslanda@gmail.com>
2026-05-15 03:31:05 -0500

    add botones.css

--- app/views/empleados/reportes.php
+++ app/views/empleados/reportes.php
@@ -8,61 +8,73 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/css/dashboard.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/css/table-responsive.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/css/botones.css">
 </head>
 
 <body>
 
-<?php include __DIR__ . '/../layouts/sidebar-dashboard.php'; ?>
+    <?php include __DIR__ . '/../layouts/sidebar-dashboard.php'; ?>
 
-<!-- CONTENIDO PRINCIPAL -->
-<main>
-    <nav class="breadcrumb">
-        <span>Dashboard</span>
-        <i class="fa-solid fa-chevron-right"></i>
-        <span>Empleados</span>
-        <i class="fa-solid fa-chevron-right"></i>
-        <span id="breadcrumb-page">Reportes</span>
-    </nav>
-    <div class="main-content">
-        <div class="table-responsive">
-            <?php if(empty($empleados)) : ?>
-                  <p>No hay registro</p>
-            <?php else: ?>    
-            <table class="table table-bordered text-center align-middle">
-                <thead class="table-dark">
-                    <tr>
-                        <th>ID</th>
-                        <th>Nombre</th>
-                        <th>Apellido</th>
-                        <th>DNI</th>
-                        <th>Celular</th>
-                        <th>Correo</th>
-                        <th>Cargo</th>
-                        <th>Fecha de Registro</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach($empleados as $empleaditos): ?>
-                        <tr>
-                            <td><?php echo $empleaditos['id_empleado']?></td>
-                            <td><?php echo $empleaditos['nombre']?></td>
-                            <td><?php echo htmlspecialchars($empleaditos['apellido'])?></td>
-                            <td><?php echo htmlspecialchars($empleaditos['dni'])?></td>
-                            <td><?php echo htmlspecialchars($empleaditos['celular'])?></td>
-                            <td><?php echo htmlspecialchars($empleaditos['correo'])?></td>
-                            <td><?php echo htmlspecialchars($empleaditos['id_cargo'])?></td>
-                            <td><?php echo htmlspecialchars($empleaditos['fecha_registro'])?></td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-            <?php endif; ?>
+    <!-- CONTENIDO PRINCIPAL -->
+    <main>
+        <nav class="breadcrumb">
+            <span>Dashboard</span>
+            <i class="fa-solid fa-chevron-right"></i>
+            <span>Empleados</span>
+            <i class="fa-solid fa-chevron-right"></i>
+            <span id="breadcrumb-page">Reportes</span>
+        </nav>
+        <div class="main-content">
+            <div class="table-responsive">
+                <?php if (empty($empleados)) : ?>
+                    <p>No hay registro</p>
+                <?php else: ?>
+                    <table class="table table-bordered text-center align-middle">
+                        <thead class="table-dark">
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th>DNI</th>
+                                <th>Celular</th>
+                                <th>Correo</th>
+                                <th>Cargo</th>
+                                <th>Fecha de Registro</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($empleados as $empleaditos): ?>
+                                <tr>
+                                    <td><?php echo $empleaditos['id_empleado'] ?></td>
+                                    <td><?php echo $empleaditos['nombre'] ?></td>
+                                    <td><?php echo htmlspecialchars($empleaditos['apellido']) ?></td>
+                                    <td><?php echo htmlspecialchars($empleaditos['dni']) ?></td>
+                                    <td><?php echo htmlspecialchars($empleaditos['celular']) ?></td>
+                                    <td><?php echo htmlspecialchars($empleaditos['correo']) ?></td>
+                                    <td><?php echo htmlspecialchars($empleaditos['id_cargo']) ?></td>
+                                    <td><?php echo htmlspecialchars($empleaditos['fecha_registro']) ?></td>
+                                    <td>
+                                        <button class="btn-editar">
+                                            <i class="fa-solid fa-pen"></i>
+                                        </button>
+
+                                        <button class="btn-eliminar">
+                                            <i class="fa-solid fa-trash"></i>
+                                        </button>
+
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                <?php endif; ?>
+            </div>
         </div>
-    </div>
-</main>
-<script src="<?php echo BASE_URL; ?>/public/js/dashboard.js"></script>
-<!-- Bootstrap JS Bundle -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+    </main>
+    <script src="<?php echo BASE_URL; ?>/public/js/dashboard.js"></script>
+    <!-- Bootstrap JS Bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
